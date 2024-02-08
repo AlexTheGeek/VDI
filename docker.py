350ee705ad69eadb2ed808d6af26378e5d9c598c
AlexTheGeek <28763288+AlexTheGeek@users.noreply.github.com>
2024-02-08 23:07:19 +0100

    Removing the debug option in the flask

--- docker.py
+++ docker.py
@@ -34,4 +34,4 @@ def viewer():
     return render_template('viewer.html', URL_API=app.config['URL_API'])
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5001)
